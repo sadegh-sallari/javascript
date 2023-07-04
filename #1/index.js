d4a7bf6bd24fdf2dc2a2bb2bab163c8164ef57a3
sadegh salari nia <mrsadeghsn@gmail.com>
2023-07-04 22:14:07 +0330

    Variabels, Operators and Conditions

---
+++ #1/index.js
@@ -0,0 +1,39 @@
+/** @format */
+
+// function sum(num1, num2) {
+// 	if (num1 > num2) {
+// 		console.log(`${num1} > ${num2}`);
+// 	} else {
+// 		console.log(`${num1} < ${num2}`);
+// 	}
+// }
+
+// sum(4, 8);
+
+// function weekDays(day) {
+// 	switch (day) {
+// 		case "شنبه":
+// 			console.log("اولین روز هفته");
+// 			break;
+// 		case "یکشنبه":
+// 			console.log("دومین روز هفته");
+// 			break;
+
+// 		default:
+// 			console.log("invalid day");
+// 			break;
+// 	}
+// }
+
+// weekDays("دوشنبه");
+
+// let num = 1;
+
+// while (num <= 5) {
+// 	console.log(num + 1);
+// 	num++;
+// }
+
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+}
